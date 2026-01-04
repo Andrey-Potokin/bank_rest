253076ded43e4e2c7cc4bdcf91db049198097aeb
Andrey Potokin <potokin.and@yandex.ru>
2026-01-04 15:32:10 +0300

    Убрана аннотация, которая переупаковывала Page<T>

--- src/main/java/com/example/bankcards/BankCardsApplication.java
+++ src/main/java/com/example/bankcards/BankCardsApplication.java
@@ -2,10 +2,8 @@ package com.example.bankcards;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.web.config.EnableSpringDataWebSupport;
 
 @SpringBootApplication
-@EnableSpringDataWebSupport(pageSerializationMode = EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO)
 public class BankCardsApplication {
     public static void main(String[] args) {
         SpringApplication.run(BankCardsApplication.class, args);
