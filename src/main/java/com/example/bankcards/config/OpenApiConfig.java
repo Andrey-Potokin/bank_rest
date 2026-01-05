1cb32263d6e200a830e8e88865ac64fa8bea134a
Andrey Potokin <potokin.and@yandex.ru>
2026-01-05 12:25:28 +0300

    Добавлена кнопка Authorize ко всем эндпоинтам в Swagger/OpenApi

--- src/main/java/com/example/bankcards/config/OpenApiConfig.java
+++ src/main/java/com/example/bankcards/config/OpenApiConfig.java
@@ -1,6 +1,9 @@
 package com.example.bankcards.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springdoc.core.models.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -10,11 +13,22 @@ public class OpenApiConfig {
 
     @Bean
     public OpenAPI customOpenAPI() {
-        return new io.swagger.v3.oas.models.OpenAPI()
-                .info(new io.swagger.v3.oas.models.info.Info()
+        return new OpenAPI()
+                .info(new Info()
                         .title("Bank Cards API")
                         .version("1.0")
-                        .description("API для работы с банковскими картами"));
+                        .description("API для работы с банковскими картами"))
+                .addSecurityItem(new SecurityRequirement().addList("bearerAuth"))
+                .components(new io.swagger.v3.oas.models.Components()
+                        .addSecuritySchemes("bearerAuth", securityScheme()));
+    }
+
+    private SecurityScheme securityScheme() {
+        return new SecurityScheme()
+                .type(SecurityScheme.Type.HTTP)
+                .scheme("bearer")
+                .bearerFormat("JWT")
+                .name("bearerAuth");
     }
 
     @Bean
@@ -40,5 +54,4 @@ public class OpenApiConfig {
                 .pathsToMatch("/api/user/**")
                 .build();
     }
-
 }
