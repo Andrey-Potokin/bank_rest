a14bee6dbc057832831eaaf51e9cea16585f2f28
Andrey Potokin <potokin.and@yandex.ru>
2026-01-13 22:38:46 +0300

    Добавлена проверка на наличие админа в ДБ

--- src/main/resources/db/migration/4_create_admin.sql
+++ src/main/resources/db/migration/4_create_admin.sql
@@ -1,16 +1,11 @@
-BEGIN;
-
 WITH new_admin AS (
     INSERT INTO users (username, password)
     VALUES (
-        'admin', 
+        'admin',
         '$2a$12$u8rsK1AD7GUwKOr2AHMgHuKiZDIgWwRIo6sPheohnDe4mOgBy.d4u'
     )
     RETURNING id
 )
-
 INSERT INTO user_roles (user_id, role)
 SELECT id, 'ADMIN'
 FROM new_admin;
-
-COMMIT;
